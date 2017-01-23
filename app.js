daa7edf0af7f4d270e4df535d9581a889adbff7e
Venu Annamraju <venu.annamraju@imgtec.com>
2017-01-23 12:49:41 +0530

    updated

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var express = require('express')
-var app = express()
 
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello world\n');
+});
 
-app.listen(8800, function () {
-  console.log('Example app listening on port 8800!')
-})
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
